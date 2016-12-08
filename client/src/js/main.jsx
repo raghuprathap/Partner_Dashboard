cd1fc33ac7a581a7d204d173006a2cef52a8682d
Raghu Prathap <raghuprathap478@gmail.com>
2016-12-08 17:38:17 +0530

    React Dev setup initial commit

---
+++ client/src/js/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'file?name=[name].[ext]!../index.html';
+
+ReactDOM.render(
+  <h1>Hello, Raghu!</h1>,
+  document.getElementById('content')
+);
